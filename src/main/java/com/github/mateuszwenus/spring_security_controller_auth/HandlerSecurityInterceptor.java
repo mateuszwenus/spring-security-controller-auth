32ed8725d028941c97aeae831257c65365a09d47
Mateusz Wenus <mateusz.wenus@gmail.com>
2014-03-25 21:16:15 +0100

    added factory method

--- src/main/java/com/github/mateuszwenus/spring_security_controller_auth/HandlerSecurityInterceptor.java
+++ src/main/java/com/github/mateuszwenus/spring_security_controller_auth/HandlerSecurityInterceptor.java
@@ -1,11 +1,17 @@
 package com.github.mateuszwenus.spring_security_controller_auth;
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.SecurityMetadataSource;
 import org.springframework.security.access.intercept.AbstractSecurityInterceptor;
 import org.springframework.security.access.intercept.InterceptorStatusToken;
+import org.springframework.security.access.vote.AffirmativeBased;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,8 +28,8 @@ public class HandlerSecurityInterceptor extends AbstractSecurityInterceptor impl
 
   @Override
   public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-    if (handler instanceof HandlerMethod){
-      HandlerInvocation hi = new HandlerInvocation(req, resp, (HandlerMethod)handler);
+    if (handler instanceof HandlerMethod) {
+      HandlerInvocation hi = new HandlerInvocation(req, resp, (HandlerMethod) handler);
       InterceptorStatusToken token = super.beforeInvocation(hi);
       tokenTL.set(token);
     }
@@ -52,5 +58,15 @@ public class HandlerSecurityInterceptor extends AbstractSecurityInterceptor impl
   @Override
   public SecurityMetadataSource obtainSecurityMetadataSource() {
     return securityMetadataSource;
+  }
+
+  @SuppressWarnings("rawtypes")
+  public static HandlerSecurityInterceptor create() {
+    HandlerSecurityInterceptor interceptor = new HandlerSecurityInterceptor();
+    WebExpressionVoterAdapter voter = new WebExpressionVoterAdapter(new WebExpressionVoter());
+    AccessDecisionManager accessDecisionManager = new AffirmativeBased(Arrays.<AccessDecisionVoter> asList(voter));
+    interceptor.setAccessDecisionManager(accessDecisionManager);
+    interceptor.setSecurityMetadataSource(new ExpressionBasedHandlerInvocationSecurityMetadataSource());
+    return interceptor;
   }
 }
